9d6b4acafc6e8256ff365f490425afd1acee5b63
Trương Nguyễn <truongnguyen1582000>
2020-09-29 14:07:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <div className="app-sub">
+        <h2>Sub</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
